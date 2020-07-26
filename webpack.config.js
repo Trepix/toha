b8349bd3e098a210e77959cded01e3569dffa3eb
Pau Trepat Segura <pau.trepat.segura@gmail.com>
2020-07-26 04:01:01 +0200

    Generate a common blocking css to avoid flickering

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-const common_assets = ['./static/assets/css/icons.css',
-  './static/assets/css/main.css',
-  './static/assets/css/navbar.css',
-  './static/assets/js/navbar.js'];
-
 module.exports = {
     entry: {
+      common: [
+        './static/assets/css/icons.css',
+        './static/assets/css/main.css',
+        './static/assets/css/navbar.css',
+        './static/assets/js/navbar.js'
+      ],
       homepage: [
         './static/assets/css/about.css',
         './static/assets/css/achievements.css',
@@ -18,18 +19,18 @@ module.exports = {
         './static/assets/css/recent-posts.css',
         './static/assets/css/skills.css',
         './static/assets/js/home.js',
-      ].concat(common_assets),
+      ],
       blog: [
         './static/assets/css/list.css',
         './static/assets/js/list.js'
-      ].concat(common_assets),
+      ],
       post: [
         "./static/assets/css/single.css",
         "./static/assets/js/single.js"
-      ].concat(common_assets),
+      ],
       404: [
         "./static/assets/css/404.css"
-      ].concat(common_assets)
+      ]
     },
     output: {
       path: path.resolve('./static/assets/dist'),
